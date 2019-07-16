4595c6459705ed8c0b37e90564da91d900b80174
fangziqiang <286330540@qq.com>
2019-07-16 16:33:52 +0800

    添加oppo会员自动签到领积分

--- src/com/oppojifen/test_Usercenter.py
+++ src/com/oppojifen/test_Usercenter.py
@@ -19,38 +19,44 @@ class testUsercenter(unittest.TestCase):
         desired_caps['platformVersion'] = '7.1.1'
         desired_caps['deviceName'] = 'ba36aa7a'
 #       微信
-        desired_caps['appPackage'] = 'com.tencent.mm'
-        desired_caps['appActivity'] = '.ui.LauncherUI'
+        desired_caps['appPackage'] = 'com.oppo.usercenter'
+        desired_caps['appActivity'] = 'com.oppo.usercenter.vip.UCVIPMainActivity'
 
         #设置每次启动不清除程序原始数据
         desired_caps['noReset'] = 'True'
         pass
         # 初始化Appium 连接
-        driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
 
     #   测试用例使用test开头
-    def testbaidu(self):
-        self.driver.get("http://www.baidu.com")
-        self.driver.find_element_by_id("kw").send_keys("selenium")
-        self.driver.find_element_by_id("su").click()
-        text = self.driver.find_element_by_xpath(".//*[@id='1']/h3/a").get_attribute("text")
-        print(text)
+    def getintegral(self):
+        signin = self.driver.find_element_by_id("com.oppo.usercenter:id/cb_sign_in")
+        signin.click()
+        signinButton = self.driver.find_element_by_id("com.oppo.usercenter:id/sign_btn")
+        is_signed = signinButton.text
+        if signinButton.is_enabled():
+            signinButton.click()
+        #签到成功弹窗
+#       text = self.driver.find_element_by_id("com.oppo.usercenter:id/get_btn").text
+#       print(text)
+        else:
+            print(is_signed)
+       
 
         #   断言判断文本是否存在于页面中
-        self.assertIn("Web Browser Automation",text)
+#         self.assertIn("Web Browser Automation",text)
 
     #   添加teardown进行善后处理
-    def tearDown(self):
-        self.driver.quit()
+#     def tearDown(self):
+#         self.driver.quit()
 
 #   添加测试集合
 suit = unittest.TestSuite()
-suit.addTest(testUsercenter("testbaidu"))
+suit.addTest(testUsercenter("getintegral"))
 
 if __name__ == '__main__':
     #  使用main()方法进行运行用例
     # unittest.main()
-
     #   使用 run放进行运行测试用例集
     run = unittest.TextTestRunner()
     run.run(suit)
