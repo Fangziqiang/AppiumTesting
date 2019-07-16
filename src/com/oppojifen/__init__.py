9420c5dc58941273e0c9ade3a928431a78992c70
fangziqiang <286330540@qq.com>
2019-07-16 14:00:08 +0800

    121212121

---
+++ src/com/oppojifen/__init__.py
@@ -0,0 +1,15 @@
+#需要领积分的应用
+#我的oppo
+#1、com.oppo.usercenter/com.oppo.usercenter.vip.UCVIPMainActivity
+#oppo+
+#2、  com.oppo.community/com.oppo.community.MainActivity
+#3、主题商店com.nearme.themespace/com.nearme.themespace.activities.ThemeMainActivity
+#4、 com.oppo.market/a.a.a.agk
+#5、 com.oppo.market/a.a.a.agk}
+#6、 com.nearme.gamecenter/a.a.a.avi
+#7、 com.coloros.bbs/com.coloros.bbs.modules.main.BBSMainActivty}
+#8、 com.coloros.yoli/com.coloros.yoli.maintab.ui.MainTabActivity
+#9、 com.android.browser/com.android.browser.BrowserActivity
+#
+#
+#
